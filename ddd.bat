0637ec91263898d93d4df5fc5b1682852f0ad7ee
Lucas Ventura <lucas.ventura@webfleet.com>
2021-12-16 09:41:44 +0100

    Improved installation process

--- ddd.bat
+++ ddd.bat
@@ -2,7 +2,9 @@
 setlocal enableDelayedExpansion
 set dodaemon=%~dp0\dodaemon.exe
 %dodaemon% isregd || (
-    echo "dodaemon" WSL distribution not installed, trying to install with the provided `ext4.vhdx` file
-    echo y | %dodaemon% >nul
+    echo "dodaemon" WSL distribution not installed
+    @REM echo y | %dodaemon% >nul
+    %dodaemon% install
+    del /F %~dp0\install.ext4.vhdx
 )
 %dodaemon% run sh -c "socat TCP-LISTEN:2375,reuseaddr,fork UNIX-CLIENT:/var/run/docker.sock & dockerd"
